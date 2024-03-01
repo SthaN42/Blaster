6d88474b8d208c7212b90ada9039d4e92e5cebe1
Nathan Guerin <nathan.guerin87@gmail.com>
2024-03-01 14:59:53 +0100

    Feat: Projectile weapon class

---
+++ Source/Blaster/Weapon/ProjectileWeapon.h
@@ -0,0 +1,17 @@
+// Copyright Nathan Guerin
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon.h"
+#include "ProjectileWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API AProjectileWeapon : public AWeapon
+{
+	GENERATED_BODY()
+	
+};
