10e290cf6276fe27a899aba5f5c4574f3fe9deaa
Nathan Guerin <nathan.guerin87@gmail.com>
2024-03-05 20:11:31 +0100

    Feat: Shell physics

--- Source/Blaster/Weapon/Casing.cpp
+++ Source/Blaster/Weapon/Casing.cpp
@@ -3,16 +3,34 @@
 
 #include "Casing.h"
 
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
+
 ACasing::ACasing()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
 	CasingMesh = CreateDefaultSubobject<UStaticMeshComponent>("CasingMesh");
 	SetRootComponent(CasingMesh);
+	CasingMesh->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
+	CasingMesh->SetSimulatePhysics(true);
+	CasingMesh->SetNotifyRigidBodyCollision(true);
+	CasingMesh->SetEnableGravity(true);
 }
 
 void ACasing::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	CasingMesh->OnComponentHit.AddDynamic(this, &ThisClass::OnHit);
+	CasingMesh->AddImpulse(GetActorForwardVector() * ShellEjectionImpulse);
+}
+
+void ACasing::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (ShellSound)
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, ShellSound, GetActorLocation());
+	}
+	Destroy();
 }
--- Source/Blaster/Weapon/Casing.h
+++ Source/Blaster/Weapon/Casing.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Actor.h"
 #include "Casing.generated.h"
 
+class USoundCue;
+
 UCLASS()
 class BLASTER_API ACasing : public AActor
 {
@@ -17,7 +19,16 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	UFUNCTION()
+	virtual void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+	
 private:
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<UStaticMeshComponent> CasingMesh;
+
+	UPROPERTY(EditAnywhere)
+	float ShellEjectionImpulse = 5.f;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<USoundCue> ShellSound;
 };
