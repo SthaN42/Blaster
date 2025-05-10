7ad6db3d630081b8c2c22f9a9bde971d4a5b3677
Nathan Guerin <nathan.guerin87@gmail.com>
2025-05-10 17:30:01 +0200

    Feat: Better Player Start Selection

--- Source/Blaster/GameMode/BlasterGameMode.cpp
+++ Source/Blaster/GameMode/BlasterGameMode.cpp
@@ -25,10 +25,36 @@ void ABlasterGameMode::RequestRespawn(ACharacter* EliminatedCharacter, AControll
 	}
 	if (EliminatedController)
 	{
+		// Find the PlayerStart that is the most isolated from the other players
 		TArray<AActor*> PlayerStarts;
 		UGameplayStatics::GetAllActorsOfClass(this, APlayerStart::StaticClass(), PlayerStarts);
-		const int32 Selection = FMath::RandRange(0, PlayerStarts.Num() - 1);
 		
-		RestartPlayerAtPlayerStart(EliminatedController, PlayerStarts[Selection]);
+		TArray<AActor*> Players;
+		UGameplayStatics::GetAllActorsOfClass(this, ABlasterCharacter::StaticClass(), Players);
+
+		AActor* SelectedPlayerStart = PlayerStarts[FMath::RandRange(0, PlayerStarts.Num() - 1)];
+		double SelectedPlayerStartDistance= 0.0;
+		
+		for (AActor* PlayerStart : PlayerStarts)
+		{
+			FVector PlayerStartLocation = PlayerStart->GetActorLocation();
+			double ClosestDistance = TNumericLimits<double>::Max();
+			
+			for (const AActor* Player : Players)
+			{
+				const double Distance = (Player->GetActorLocation() - PlayerStartLocation).Length();
+				if (Distance < ClosestDistance)
+				{
+					ClosestDistance = Distance;
+				}
+			}
+			if (ClosestDistance >= SelectedPlayerStartDistance)
+			{
+				SelectedPlayerStartDistance = ClosestDistance;
+				SelectedPlayerStart = PlayerStart;
+			}
+		}
+		
+		RestartPlayerAtPlayerStart(EliminatedController, SelectedPlayerStart);
 	}
 }
